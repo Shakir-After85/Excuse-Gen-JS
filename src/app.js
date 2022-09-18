467270c740811b19e57ad8dbcfb53b955478ed7f
Shakir-After85 <shakir.tamar@gmail.com>
2022-09-18 01:15:27 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let who = ["The snake", "My job", "Her cat", "My bird"];
+  let action = ["jumped", "fired", "shredded", "broke"];
+  let what = ["in my boot", "me", "the couch", "the window"];
+  let when = [
+    "last night",
+    "said I was stealing boxes",
+    "when I finished eating",
+    "last night",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
